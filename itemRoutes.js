71420ff4f02e8bf0086281282b1dab5a696db82d
Evan Hesketh <evanhesketh@yahoo.ca>
2023-04-05 19:53:41 -0400

    add patch and delete routes

--- itemRoutes.js
+++ itemRoutes.js
@@ -15,36 +15,63 @@ const { BadRequestError, NotFoundError } = require("./expressError");
   ...
 ]}
 */
-router.get('/', function (req, res) {
+router.get("/", function (req, res) {
   return res.json(db);
 });
 
 /** POST /items/:
  *
  * returns { added:{ name: "popsicle", price: 1.45 } }
-*/
+ */
 router.post("/", function (req, res) {
   if (req.body === undefined) throw new BadRequestError();
   const item = req.body;
   db.items.push(item);
-  return res.json({ added: item });
+  return res.status(201).json({ added: item });
 });
 
+/** GET /items/:name: returns item {name: "popsicle", "price": 1.45} or 404. */
 
-/** GET /items/:name: returns item {item} or 404. */
-
-router.get("/:name", function (req, res, next) {
-  const item = db.items.find(item => item.name === req.params.name);
-  if (!item){
+router.get("/:name", function (req, res) {
+  const item = db.items.find((item) => item.name === req.params.name);
+  if (!item) {
     throw new NotFoundError();
   }
   return res.json(item);
 });
 
+/** PATCH /items/:name:
+ * input: {name: "new popsicle", price: 2.45}
+ * returns: {updated: {name: "new popsicle", price: 2.45}} or 404
+ */
 
+router.patch("/:name", function (req, res) {
+  let item = db.items.find((item) => item.name === req.params.name);
+  if (!item) {
+    throw new NotFoundError();
+  }
 
+  const name = req.body.name || item.name;
+  const price = req.body.price === undefined ? item.price : req.body.price;
 
+  item.name = name;
+  item.price = price;
 
+  return res.json({ updated: item });
+});
+
+/** DELETE /items/:name: returns {message: "Deleted"} or 404*/
+
+router.delete("/:name", function (req, res) {
+  const itemIdx = db.items.findIndex((item) => item.name === req.params.name);
+  console.log("itemIndex", itemIdx);
+  if (itemIdx === undefined) {
+    throw new NotFoundError();
+  }
+
+  db.items.splice(itemIdx, 1);
+
+  return res.json({ message: "Deleted" });
+});
 
 module.exports = router;
-
