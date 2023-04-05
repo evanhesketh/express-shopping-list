79807ea481002774111a25c6ac6fdc0d809df9f7
Madelyn Romberg <madelynromberg@g.ucla.edu>
2023-04-05 16:11:54 -0700

    allow route to POST /items/

--- itemRoutes.js
+++ itemRoutes.js
@@ -5,16 +5,31 @@ const express = require("express");
 const db = require("./fakeDb");
 const router = new express.Router();
 
+const { BadRequestError } = require("./expressError");
+
 /** GET /items/:
  *
  * returns { items: [
   { name: "popsicle", price: 1.45 },
-  { name: "cheerios", price: 3.40 }
+  { name: "cheerios", price: 3.40 },
+  ...
 ]}
 */
-router.get('/', function(req, res) {
+router.get('/', function (req, res) {
   return res.json(db);
 });
+
+/** POST /items/:
+ *
+ * returns { added:{ name: "popsicle", price: 1.45 } }
+*/
+router.post("/", function (req, res) {
+  if (req.body === undefined) throw new BadRequestError();
+  const item = req.body;
+  db.items.push(item);
+  return res.json({ added: item });
+});
+
 
 
 
